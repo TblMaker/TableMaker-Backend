863a3526a3716c417526468c56adf96e3c365160
JoMingyu <city7310@naver.com>
2018-02-03 12:11:25 +0900

    [DONE] Config

---
+++ Server/config/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+
+class Config(object):
+    DOMAIN = None
+    PORT = 3000
+
+    SECRET_KEY = os.getenv('SECRET_KEY', '85c145a16bd6f6e1f3e104ca78c6a102')
+    # Secret key for any 3-rd party libraries
+
+    SERVICE_NAME = 'TableMaker'
+
+    SWAGGER = {
+        'title': SERVICE_NAME,
+        'specs_route': '/docs/',
+        'uiversion': 3,
+
+        'info': {
+            'title': SERVICE_NAME + ' API',
+            'version': '1.0',
+            'description': ''
+        },
+
+        'host': '{}:{}'.format(DOMAIN, PORT) if DOMAIN else None,
+        'basePath': '/ '
+    }
---
+++ Server/config/dev.py
@@ -0,0 +1,16 @@
+from config import Config
+
+
+class DevConfig(Config):
+    HOST = 'localhost'
+
+    if not Config.DOMAIN:
+        Config.SWAGGER['host'] = '{}:{}'.format(HOST, Config.PORT)
+
+    DEBUG = True
+
+    MONGODB_SETTINGS = {
+        'host': 'localhost',
+        'port': 27017,
+        'db': '{}-dev'.format(Config.SERVICE_NAME)
+    }
