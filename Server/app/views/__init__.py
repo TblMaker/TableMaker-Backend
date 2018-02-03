ba9bafbb96de1de504e3c5b8e4c6792f2846fa82
JoMingyu <city7310@naver.com>
2018-02-03 12:12:06 +0900

    [DONE] Docs, Models, Views

---
+++ Server/app/views/__init__.py
@@ -0,0 +1,27 @@
+import json
+import time
+
+from flask import Response
+from flask_restful import Resource
+
+
+class BaseResource(Resource):
+    def __init__(self):
+        self.now = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    @classmethod
+    def unicode_safe_json_response(cls, data, status_code=200):
+        return Response(
+            json.dumps(data, ensure_ascii=False),
+            status_code,
+            content_type='application/json; charset=utf8'
+        )
+
+
+class Router(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        pass
