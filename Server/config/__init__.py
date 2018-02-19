f6944bce656c073c1f8ac393399fd53a6629bc39
JoMingyu <city7310@naver.com>
2018-02-19 11:42:56 +0900

    [UPDATE] Config
    - DOMAIN to REPRESENTATIVE_HOST
    - Add configs about JWT

--- Server/config/__init__.py
+++ Server/config/__init__.py
@@ -1,12 +1,18 @@
+from datetime import timedelta
+
 import os
 
 
 class Config(object):
-    DOMAIN = None
+    REPRESENTATIVE_HOST = None
     PORT = 3000
 
     SECRET_KEY = os.getenv('SECRET_KEY', '85c145a16bd6f6e1f3e104ca78c6a102')
     # Secret key for any 3-rd party libraries
+
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=365)
+    JWT_HEADER_TYPE = 'JWT'
 
     SERVICE_NAME = 'TableMaker'
 
@@ -21,6 +27,6 @@ class Config(object):
             'description': ''
         },
 
-        'host': '{}:{}'.format(DOMAIN, PORT) if DOMAIN else None,
+        'host': '{}:{}'.format(REPRESENTATIVE_HOST, PORT) if REPRESENTATIVE_HOST else None,
         'basePath': '/ '
     }
